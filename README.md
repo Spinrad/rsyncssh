5e9e9e4b6202579f342588bc9e0adaba7a16b167
spinrad <bonnevialle@gmail.com>
2014-09-05 16:39:24 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+A simple bash script for a weekly incremental backup of a folder with rsync over ssh, and a mail notification..
+It use hardlinks for saving space
---
+++ rsyncssh.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# This script runs incremental backups of a specified folder, keep logs and send a mail notification.
+# Dependencies : rsync, ssh, and mail
+# Launch script from destination host, because  --link-dest need a localpath)
+# 
+###Setup###
+PREFIX="HostSource"
+DEST="/home/user/mybackup"
+SOURCE="user@server:/folder_to_backup/"
+DATE=`date '+%F_%H-%M'`
+NAS="HostDest"
+MAIL=user@mail.org
+##########
+#
+echo "$PREFIX Rsync incremental backup script"
+echo "$PREFIX Destination directory: $NAS $DEST"
+echo "$PREFIX Source directory: $SOURCE"
+ 
+# Verify that the destination folder exists
+if [ ! -d "$DEST" ]; then
+    echo "$PREFIX Destination folder \"$DEST\" not found. Exiting..."
+    exit 1
+fi
+ 
+cd "$DEST"
+ 
+echo "$PREFIX Step 1: Rotating old backups..."
+ 
+rm -rf backup.5
+mv backup.4 backup.5 2>/dev/null
+mv backup.3 backup.4 2>/dev/null
+mv backup.2 backup.3 2>/dev/null
+mv backup.1 backup.2 2>/dev/null
+mv backup backup.1 2>/dev/null
+mv current backup 2>/dev/null
+ 
+echo "$PREFIX Done!"
+echo "$PREFIX Step 2: Running backup..."
+sleep 1
+ 
+#Launch script from destination host, because  --link-dest need a localpath)
+rsync -v -a --delete --log-file="$DEST/$PREFIX-$DATE.log" --link-dest="../backup/" "$SOURCE" "$DEST/current"
+ 
+echo "$PREFIX Done!"
+sleep 1
+echo "$PREFIX Step 3: Mail notification..."
+echo "$DATE - Backup rsync $PREFIX $Source to $NAS@$DEST : OK" | mail -s "Daily backup rsync $PREFIX to $NAS : OK" "$MAIL";
+echo "$PREFIX Done!"
